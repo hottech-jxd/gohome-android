2eaba000c571b6df822a76b705df02fd5383a0ce
hottech-jxd <51818549@qq.com>
2019-01-02 18:05:14 +0800

    add

---
+++ quanmodule/src/main/java/com/jxd/android/gohomeapp/quanmodule/adapter/DataAdapter.kt
@@ -0,0 +1,13 @@
+package com.jxd.android.gohomeapp.quanmodule.adapter
+
+import com.jxd.android.gohomeapp.quanmodule.R
+
+
+class DataAdapter(data: List<String>) :BaseQuickAdapter<String , BaseViewHolder>( R.layout.layout_goods_item_1 , data) {
+
+
+
+    override fun convert(helper: BaseViewHolder?, item: String) {
+
+    }
+}
